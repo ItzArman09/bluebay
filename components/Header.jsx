67735a8f1517539e93f2c354405e40fc82e347e9
ItzArman09 <100463136+ItzArman09@users.noreply.github.com>
2024-06-04 18:04:22 +0530

    okay

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,12 +4,11 @@ import Navigation from "./Navigation";
 
 export default function Header() {
     return (
-
         <div className="sticky inset-x-0 top-0 z-10 bg-opacity-60 bg-zinc-100 backdrop-blur-lg">
-            <div className="container flex items-center justify-between py-8 ">
-
-                <Image src={logo} alt="logo"  className="w-40 h-20" />
-
+            <div className="container flex items-center justify-between py-8">
+                <div className="w-20 h-20 rounded-full overflow-hidden flex items-center justify-center">
+                    <Image src={logo} alt="logo" width={80} height={80} className="object-cover" />
+                </div>
                 <Navigation />
             </div>
         </div>
